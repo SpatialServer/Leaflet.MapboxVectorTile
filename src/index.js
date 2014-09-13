7dd98baa50dbe139931d30cb0726296459d90bd7
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-09-12 17:11:13 -0700

    unnecessary file

--- src/index.js
+++
@@ -1,5 +0,0 @@
-var VectorTile = require('vector-tile').VectorTile;
-var VectorTileFeature = require('vector-tile').VectorTileFeature;
-var VectorTileLayer = require('vector-tile').VectorTileLayer;
-var Protobuf = require('pbf');
-var Point = require('point-geometry');
