25204bffb7cac603b4d4357017c32d1991ab4e47
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-09-14 12:52:11 -0700

    test that creates a map and makes sure a tile loads

--- test/js/MVTSource.test.js
+++ test/js/MVTSource.test.js
@@ -13,3 +13,19 @@ test('creating MVTSource object', function(t) {
   t.ok(mvtSource, 'gadm2014kenya with static labels source created');
 });
 
+test('create a map, loading a tile', function(t) {
+  document.body.innerHTML += '<div id="map"></div>';
+  var map = L.map('map').setView([0,39], 6); // africa
+  var mvtSource = new MVTSource(basicOpts);
+  L.tileLayer('http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 18
+  }).addTo(map);
+  map.addLayer(mvtSource);
+
+  t.plan(1);
+  setTimeout(function() {
+    t.ok(mvtSource.loadedTiles['6:38:32'], 'tile 6:38:32 loaded');
+  }, 1000);
+});
+
+
