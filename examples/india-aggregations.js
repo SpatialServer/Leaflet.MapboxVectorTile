efbd7521acc396786742dbda07f4ba06b40a03ae
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-09-22 15:12:46 -0700

    able to get fsp india gaul vectiles via mapbox api

---
+++ examples/india-aggregations.js
@@ -0,0 +1,82 @@
+var debug = {};
+
+var map = L.map('map').setView([25.40,79.409], 6); // Northern India
+
+L.tileLayer('http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  maxZoom: 18
+}).addTo(map);
+
+
+var mvtSource = new L.TileLayer.MVTSource({
+  url: "https://a.tiles.mapbox.com/v4/nicholashallahan.43cc7605/{z}/{x}/{y}.vector.pbf?access_token=pk.eyJ1IjoibmljaG9sYXNoYWxsYWhhbiIsImEiOiJ5YWxaRUY0In0.qLtNgKJKXvhm7j5u6ZvDDw",
+  debug: true,
+  clickableLayers: ['gaul_2014_adm1'],
+  getIDForLayerFeature: function(feature) {
+    return feature.properties.id;
+  },
+
+  /**
+   * The filter function gets called when iterating though each vector tile feature (vtf). You have access
+   * to every property associated with a given feature (the feature, and the layer). You can also filter
+   * based of the context (each tile that the feature is drawn onto).
+   *
+   * Returning false skips over the feature and it is not drawn.
+   *
+   * @param feature
+   * @returns {boolean}
+   */
+  filter: function(feature, context) {
+    if (feature.layer.name === 'gaul_2014_adm1') {
+      return true;
+    }
+    return false;
+  },
+
+  style: function (feature) {
+    var style = {};
+
+    var type = feature.type;
+    switch (type) {
+      case 1: //'Point'
+        style.color = 'rgba(49,79,79,1)';
+        style.radius = 5;
+        style.selected = {
+          color: 'rgba(255,255,0,0.5)',
+          radius: 6
+        };
+        break;
+      case 2: //'LineString'
+        style.color = 'rgba(161,217,155,0.8)';
+        style.size = 3;
+        style.selected = {
+          color: 'rgba(255,25,0,0.5)',
+          size: 4
+        };
+        break;
+      case 3: //'Polygon'
+        style.color = fillColor;
+        style.outline = {
+          color: strokeColor,
+          size: 1
+        };
+        style.selected = {
+          color: 'rgba(255,140,0,0.3)',
+          outline: {
+            color: 'rgba(255,140,0,1)',
+            size: 2
+          }
+        };
+        break;
+    }
+    return style;
+  }
+
+});
+debug.mvtSource = mvtSource;
+
+//Globals that we can change later.
+var fillColor = 'rgba(149,139,255,0.4)';
+var strokeColor = 'rgb(20,20,20)';
+
+//Add layer
+map.addLayer(mvtSource);
