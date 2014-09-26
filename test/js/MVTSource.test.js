111e87e204255d2e16e11c711943ff9736fede54
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-09-25 18:34:03 -0700

    commenting back in all tests

--- test/js/MVTSource.test.js
+++ test/js/MVTSource.test.js
@@ -14,59 +14,59 @@ test('creating MVTSource object', function(t) {
   t.ok(mvtSource, 'gadm2014kenya with static labels source created');
 });
 
-//test('create a map, loading a tile', function(t) {
-//  var basicOpts = require('../fixtures/basicStaticLabel.js');
-//  document.body.innerHTML += '<div id="map"></div>';
-//  var map = L.map('map').setView([0,39], 6); // africa
-//  var mvtSource = new MVTSource(basicOpts);
-//  L.tileLayer('http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-//    maxZoom: 18
-//  }).addTo(map);
-//  map.addLayer(mvtSource);
-//
-//  t.plan(1);
-//  setTimeout(function() {
-//    t.ok(mvtSource.loadedTiles['6:38:32'], 'tile 6:38:32 loaded');
-//  }, 1000);
-//});
-//
-//test('basic map, zoom in, zoom out, check for drawn tiles', function(t) {
-//  var basicOpts = require('../fixtures/basicStaticLabel.js');
-//  document.body.innerHTML += '<div id="map"></div>';
-//  var map = L.map('map').setView([0,39], 6); // africa
-//  var mvtSource = new MVTSource(basicOpts);
-//  L.tileLayer('http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-//    maxZoom: 18
-//  }).addTo(map);
-//  map.addLayer(mvtSource);
-//
-//  map.zoomIn();
-//  map.zoomOut();
-//
-//  t.plan(1);
-//  setTimeout(function() {
-//    t.ok(mvtSource.loadedTiles['6:38:32'], 'tile 6:38:32 loaded');
-//  }, 1000);
-//});
-//
-//test('create a confetti map, loading a confetti tile', function(t) {
-//  var confetti = require('../fixtures/confetti_datasource.js');
-//  document.body.innerHTML += '<div id="map"></div>';
-//  var map = L.map('map').setView([26.85305,80.93765], 14); // india
-//  var mvtSource = new MVTSource(confetti);
-//  L.tileLayer('http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-//    maxZoom: 18
-//  }).addTo(map);
-//  map.addLayer(mvtSource);
-//
-//  console.log("Center: " + map.getCenter());
-//
-//  t.plan(1);
-//  setTimeout(function() {
+test('create a map, loading a tile', function(t) {
+  var basicOpts = require('../fixtures/basicStaticLabel.js');
+  document.body.innerHTML += '<div id="map"></div>';
+  var map = L.map('map').setView([0,39], 6); // africa
+  var mvtSource = new MVTSource(basicOpts);
+  L.tileLayer('http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 18
+  }).addTo(map);
+  map.addLayer(mvtSource);
+
+  t.plan(1);
+  setTimeout(function() {
+    t.ok(mvtSource.loadedTiles['6:38:32'], 'tile 6:38:32 loaded');
+  }, 1000);
+});
+
+test('basic map, zoom in, zoom out, check for drawn tiles', function(t) {
+  var basicOpts = require('../fixtures/basicStaticLabel.js');
+  document.body.innerHTML += '<div id="map"></div>';
+  var map = L.map('map').setView([0,39], 6); // africa
+  var mvtSource = new MVTSource(basicOpts);
+  L.tileLayer('http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 18
+  }).addTo(map);
+  map.addLayer(mvtSource);
+
+  map.zoomIn();
+  map.zoomOut();
+
+  t.plan(1);
+  setTimeout(function() {
+    t.ok(mvtSource.loadedTiles['6:38:32'], 'tile 6:38:32 loaded');
+  }, 1000);
+});
+
+test('create a confetti map, loading a confetti tile', function(t) {
+  var confetti = require('../fixtures/confetti_datasource.js');
+  document.body.innerHTML += '<div id="map"></div>';
+  var map = L.map('map').setView([26.85305,80.93765], 14); // india
+  var mvtSource = new MVTSource(confetti);
+  L.tileLayer('http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 18
+  }).addTo(map);
+  map.addLayer(mvtSource);
+
+  console.log("Center: " + map.getCenter());
+
+  t.plan(1);
+  setTimeout(function() {
 //    console.log("# Tiles: " + Object.keys(mvtSource.loadedTiles).length);
-//    t.ok(mvtSource.loadedTiles['14:11875:6922'], 'tile 14:11875:6922 loaded');
-//  }, 1000);
-//});
+    t.ok(mvtSource.loadedTiles['14:11875:6922'], 'tile 14:11875:6922 loaded');
+  }, 1000);
+});
 
 
 test('ensure labels are removed when mvtSource is removed from map', function(t) {
@@ -82,7 +82,7 @@ test('ensure labels are removed when mvtSource is removed from map', function(t)
   t.plan(2);
   setTimeout(function() {
     var layersWithLabels = Object.keys(map._layers).length;
-    t.equal(layersWithLabels, 10, 'should be 10 layers on map');
+    t.equal(layersWithLabels, 41, 'should be 41 layers on map');
     setTimeout(function() {
       map.removeLayer(mvtSource);
       var layersWithLabels = Object.keys(map._layers).length;
