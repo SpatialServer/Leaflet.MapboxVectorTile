fea0e34e0705eb7dd28a445a1944691ae5fbb818
Henry Tung <henryptung@gmail.com>
2015-08-05 19:08:58 -0700

    Update LICENSE to match ISC license template
    Also add license comment to built dist and leave license comments
    in built dist.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2014, Spatial Development International
+ * All rights reserved.
+ *
+ * Source code can be found at:
+ * https://github.com/SpatialServer/Leaflet.MapboxVectorTile
+ *
+ * @license ISC
+ */
+
+module.exports = require('./MVTSource');
